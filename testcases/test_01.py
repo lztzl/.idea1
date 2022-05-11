b36f9d6d046806c95759e5ea9c0ba9df61f0a93f
helloasimo <3396489052@qq.com>
2022-05-11 16:18:16 +0800

    change9

--- testcases/test_01.py
+++ testcases/test_01.py
@@ -1,2 +1,21 @@
+# 单元测试
+# test_get_level.py
+import pytest
+
+from main import *
+
+
+class GetLevel(pytest.TestCase):
+
+    def test_get_level1(self):
+        self.assertEquals(get_level(90), "优秀")
+
+    def test_get_level2(self):
+        self.assertEquals(get_level(80), "良好")
+
+
+if __name__ == '__main__':
+    pytest.main(verbosity=2)
+
 
 
