4acc0cddf9a4d5711d31049dde3a1133af7ce3c3
helloasimo <3396489052@qq.com>
2022-06-02 17:15:25 +0800

    change10

--- main.py
+++ main.py
@@ -1,31 +1,63 @@
-# 切记，py文件也要以“test_”开头
-import pytest  # 首先引入pytest。简单的例子不引入也能运行
+# import math
+class Error(BaseException):
+    pass
 
+class OutOfRangeError(Error):
+    def __init__(self, message):
+        self.message = message
 
-def add(x):  # 先申明1个函数“返回传值+2”，做为测试使用
-    return x + 2
+class TriangleError(Error):
+    def __init__(self, message):
+        self.message = message
 
+def checkRange(a, b, c):
+    if a<10 or a>50:
+        raise OutOfRangeError('A is out of the given range')
+    if b<10  or b>50:
+        raise OutOfRangeError('B is out of the given range')
+    if c<10 or c>50:
+        raise OutOfRangeError('C is out of the given range')
 
-class Test_Class_Add():  # 先定义1个类，一定要以“test_”开头
-    def test_add1(self):  # 定义测试用户，一定要以“test_”开头
-        assert add(2) == 5  # 使用assert进行断言，是否相等
+def checkTriangle(a, b, c):
+    if a + b<= c or b + c<= a or c + a<= b:
+        raise TriangleError('Triangle cannot be formed with these sides')
 
-    def test_add2(self):
-        assert add(22) == 24
+def triangleType(a, b, c):
+    checkRange(a, b, c)
+    checkTriangle(a, b, c)
+    # s = (a + b+c)/2
+    # ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    # inradius = ar / s
+    if(a == b and b == c):
+        return "Equilateral Triangle"
+    elif(a == b or a == c or b == c):
+        return "Isosceles Triangle"
+    else:
+        return "Scalene Triangle"
 
-    def test_add3(self):
-        assert add(100) == 102
+def main():
+    try:
+        print("Enter the sides of the triangle in range [10-50]")
 
+        a = int(input('Enter Side A:'))
+        b = int(input('Enter Side B:'))
+        c = int(input('Enter Side C:'))
+    except ValueError as v:
+        print(v + " Raised :Input is not an integer.")
+        exit(0)
+    try:
+        checkRange(a, b, c)
+    except OutOfRangeError as e:
+        print("OutOfRangeError:" + e.message)
 
-class Test_Class_In():  # 定义第2个类
-    def test_in(self):
-        a = "Hello World！"
-        b = "Hi"
-        c = "World"
-        assert c in a  # 使用in和not in进行断言
-        assert b not in a
+    try:
+        checkTriangle(a, b, c)
+    except TriangleError as e:
+        print('TriangleError:' + e.message)
 
-    def test_notin(self):
-        assert b in a
-        assert c not in a
+    typeOfTriangle = triangleType(a, b, c)
 
+    print("The triangle is: " + typeOfTriangle)
+
+if __name__ == "__main__":
+    main()
--- testcases/test_get_level.py
+++ testcases/test_get_level.py
@@ -1,18 +1,60 @@
 
-import pytest
+
 
 from main import *
 
 
-class TestPy01():
+import pytest
+from main import triangleType
+from main import OutOfRangeError
 
+def test_bva_minm_a():
+    with pytest.raises(OutOfRangeError):
+        triangleType(9, 20, 15)
+def test_bva_minm_b():
+    with pytest.raises(OutOfRangeError):
+        triangleType(20, 9, 15)
+def test_bva_minm_c():
+    with pytest.raises(OutOfRangeError):
+        triangleType(20, 15, 9)
 
-    def test001(self):
-        result=(get_level(90), "优秀")
+def test_bva_min_a():
+    assert triangleType(10, 20, 15) == "Scalene Triangle"
+def test_bva_min_b():
+    assert triangleType(20, 10, 15) == "Scalene Triangle"
+def test_bva_min_c():
+    assert triangleType(15, 20, 10) == "Scalene Triangle"
 
-    def test002(self):
-        result=(get_level(80), "良好")
+def test_bva_minp_a():
+    assert triangleType(11, 20, 15) == "Scalene Triangle"
+def test_bva_minp_b():
+    assert triangleType(20, 11, 15) == "Scalene Triangle"
+def test_bva_minp_c():
+    assert triangleType(15, 20, 11) == "Scalene Triangle"
 
+def test_bva_maxm_a():
+    assert triangleType(49, 30, 45) == "Scalene Triangle"
+def test_bva_maxm_b():
+    assert triangleType(30, 49, 45) == "Scalene Triangle"
+def test_bva_maxm_c():
+    assert triangleType(45, 30, 49) == "Scalene Triangle"
 
-if __name__ == '__main__':
-    pytest.main(["--cov=./testcases/""--cov-report=html"])
+def test_bva_max_a():
+    assert triangleType(50, 30, 45) == "Scalene Triangle"
+def test_bva_max_b():
+    assert triangleType(30, 50, 45) == "Scalene Triangle"
+def test_bva_max_c():
+    assert triangleType(45, 30, 50) == "Scalene Triangle"
+
+def test_bva_maxp_a():
+    with pytest.raises(OutOfRangeError):
+        triangleType(51, 30, 45)
+def test_bva_maxp_b():
+    with pytest.raises(OutOfRangeError):
+        triangleType(30, 51, 45)
+def test_bva_maxp_c():
+    with pytest.raises(OutOfRangeError):
+        triangleType(30, 45, 51)
+
+def test_bva_nominal():
+    assert triangleType(12, 12, 12) == "Equilateral Triangle"
