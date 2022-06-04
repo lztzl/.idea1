9784a06dd8374d6059ee7063d40ff992684bd502
helloasimo <3396489052@qq.com>
2022-06-04 15:14:17 +0800

    change10

--- testcases/test0.py
+++ testcases/test0.py
@@ -5,55 +5,55 @@ import pytest
 from main import triangleType
 from main import OutOfRangeError
 
-def test_bva_minm_a():
+def test_mins_a():
     with pytest.raises(OutOfRangeError):
         triangleType(9, 20, 15)
-def test_bva_minm_b():
+def test_mins_b():
     with pytest.raises(OutOfRangeError):
         triangleType(20, 9, 15)
-def test_bva_minm_c():
+def test_mins_c():
     with pytest.raises(OutOfRangeError):
         triangleType(20, 15, 9)
 
-def test_bva_min_a():
+def test_min_a():
     assert triangleType(10, 20, 15) == "Scalene Triangle"
-def test_bva_min_b():
+def test_min_b():
     assert triangleType(20, 10, 15) == "Scalene Triangle"
-def test_bva_min_c():
+def test_min_c():
     assert triangleType(15, 20, 10) == "Scalene Triangle"
 
-def test_bva_minp_a():
+def test_minp_a():
     assert triangleType(11, 20, 15) == "Scalene Triangle"
-def test_bva_minp_b():
+def test_minp_b():
     assert triangleType(20, 11, 15) == "Scalene Triangle"
-def test_bva_minp_c():
+def test_minp_c():
     assert triangleType(15, 20, 11) == "Scalene Triangle"
 
-def test_bva_maxm_a():
+def test_maxs_a():
     assert triangleType(49, 30, 45) == "Scalene Triangle"
-def test_bva_maxm_b():
+def test_maxs_b():
     assert triangleType(30, 49, 45) == "Scalene Triangle"
-def test_bva_maxm_c():
+def test_maxs_c():
     assert triangleType(45, 30, 49) == "Scalene Triangle"
 
-def test_bva_max_a():
+def test_max_a():
     assert triangleType(50, 30, 45) == "Scalene Triangle"
-def test_bva_max_b():
+def test_max_b():
     assert triangleType(30, 50, 45) == "Scalene Triangle"
-def test_bva_max_c():
+def test_max_c():
     assert triangleType(45, 30, 50) == "Scalene Triangle"
 
-def test_bva_maxp_a():
+def test_maxp_a():
     with pytest.raises(OutOfRangeError):
         triangleType(51, 30, 45)
-def test_bva_maxp_b():
+def test_maxp_b():
     with pytest.raises(OutOfRangeError):
         triangleType(30, 51, 45)
-def test_bva_maxp_c():
+def test_maxp_c():
     with pytest.raises(OutOfRangeError):
         triangleType(30, 45, 51)
 
-def test_bva_nominal():
+def test_nominal():
     assert triangleType(12, 12, 12) == "Equilateral Triangle"
 
 
